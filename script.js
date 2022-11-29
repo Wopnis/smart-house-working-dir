1ea81bcdae40877af47d8d78bc14801b047ec164
Wopnis <wopnis2016@outlook.com>
2022-11-29 19:56:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+// window.addEventListener('click', e => {
+//     const target = e.target;
+//     console.log(target);
+// });
+
+const dom = {
+    selectBox: document.querySelector('.selectBox'),
+    rooms: document.querySelector('.rooms'),
+    eachRoom: document.querySelectorAll('.room'),
+    selectboxItem: document.querySelectorAll('.selectbox__item'),
+    mainElem: document.body,
+    selectBoxList: document.querySelector('.selectBox__list'),
+};
+
+const roomsList = {
+    all: 'Комнаты',
+    livingroom: 'Зал',
+    bedroom: 'Спальная',
+    kitchen: 'Кухня',
+    bathroom: 'Ванная',
+    studio: 'Кабинет',
+    washingroom: 'Уборная',
+};
+
+dom.selectBox.querySelector('.selectBox__selected').addEventListener('click', e => {
+    const target = e.target;
+    dom.selectBox.classList.toggle('open');
+});
+
+dom.mainElem.addEventListener('click', e => {
+    const target = e.target;
+    if (
+        !target.matches('.selectBox') &&
+        !target.parentElement.matches('.selectBox') &&
+        !target.parentElement.parentElement.matches('.selectBox')
+    ) {
+        dom.selectBox.classList.remove('open');
+    }
+});
+
+dom.selectBoxList.addEventListener('click', e => {
+    const target = e.target;
+    if (target.matches('.selectbox__item')) {
+        const room = target.dataset.room;
+        const activeItem = document.querySelector('.selected');
+        try {
+            activeItem.classList.remove('selected');
+        } catch (error) {
+            console.log(error.log);
+        }
+        target.classList.add('selected');
+        dom.selectBox.classList.remove('open');
+        selectOfRoom(room);
+    }
+});
+
+function selectOfRoom(room) {
+    const selectedRoom = dom.rooms.querySelector('.selected');
+    const selectedItemOfSelectBoxList = dom.selectBox.querySelector('.selectbox__item.selected');
+    if (selectedRoom) {
+        selectedRoom.classList.remove('selected');
+        try {
+            selectedItemOfSelectBoxList.classList.remove('selected');
+        } catch (error) {
+            error.log;
+        }
+    }
+    if (room !== 'all') {
+        const newSelectedRoom = dom.rooms.querySelector(`[data-room=${room}]`);
+        newSelectedRoom.classList.add('selected');
+        // console.log(newSelectedRoom);
+    }
+
+    const newSelectedListItem = dom.selectBox.querySelector(`[data-room=${room}]`);
+    try {
+        dom.selectBox.querySelector('.selected').classList.remove('selected');
+    } catch (error) {
+        error.log;
+    }
+    try {
+        newSelectedListItem.classList.add('selected');
+    } catch (error) {
+        error.log;
+    }
+    // console.log(newSelectedListItem);
+
+    const selectBoxSelectedText = dom.selectBox.querySelector('.selectBox__selected span');
+    selectBoxSelectedText.innerText = roomsList[room];
+}
+
+dom.eachRoom.forEach(room => {
+    room.addEventListener('click', e => {
+        const value = room.dataset.room;
+        selectOfRoom(value);
+    });
+});
